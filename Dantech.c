e7bf7d665a9b62137fe3b9917e2675845c5b1d56
Dantech235 <dezeugo8@gmail.com>
2023-06-18 03:35:26 -0700

    fathers day

---
+++ Dantech.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("Happy fathers Day to all fathers\n");
+	printf("wish you the very best\n");
+	printf("And to all potential fathers to be");
+
+	return (0);
+}
